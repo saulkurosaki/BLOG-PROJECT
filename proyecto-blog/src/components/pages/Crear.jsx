761eb86b7f49aaf7597446a60a8c898a644f4184
Saul Paredes <saulkurosaki13@gmail.com>
2023-06-10 02:55:15 -0600

    Form section and styling Done

--- proyecto-blog/src/components/pages/Crear.jsx
+++ proyecto-blog/src/components/pages/Crear.jsx
@@ -2,6 +2,30 @@ import React from 'react'
 
 export const Crear = () => {
   return (
-    <div>Crear</div>
+    <div className='jumbo'>
+      <h1>Crear Articulo</h1>
+      <p>Formulario para crear un articulo</p>
+
+      <form className='formulario'>
+
+        <div className='form-group'>
+          <label htmlFor='titulo'>Titulo</label>
+          <input type='text' name='titulo'/>
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor='contenido'>Contenido</label>
+          <textarea name='contenido'/>
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor='file0'>Imagen</label>
+          <input type='file' name='file0' id='file'/>
+        </div>
+
+        <input type='submit' value='Guardar' className='btn btn-success'/>
+
+      </form>
+    </div>
   )
 }
