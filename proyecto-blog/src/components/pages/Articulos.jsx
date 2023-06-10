e7ff5557ad12139183dd3504fea1dc333c43cd91
Saul Paredes <saulkurosaki13@gmail.com>
2023-06-09 21:47:44 -0600

    Articles Section structure Done

--- proyecto-blog/src/components/pages/Articulos.jsx
+++ proyecto-blog/src/components/pages/Articulos.jsx
@@ -2,6 +2,21 @@ import React from 'react'
 
 export const Articulos = () => {
   return (
-    <div>Articulos</div>
+    <>
+      <article className='articulo-item'>
+        <div className='mascara'>
+          <img src='https://tse1.mm.bing.net/th?id=OIP.Z_RedhUZ_XciZPgYbuqNqQHaF7&pid=Api&P=0&h=180' />
+        </div>
+
+        <div className='datos'>
+          <h3 className='title'>Desarrollo Web</h3>
+          <p className='description'>lalalala</p>
+
+          <button className='edit'>Editar</button>
+          <button className='delete'>Borrar</button>
+        </div>
+      </article>
+
+    </>
   )
 }
